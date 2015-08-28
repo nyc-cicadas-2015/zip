93a96d5e15d40b22ea396e968d1243356efefda1
David Tu & Alicia <pair+spacebear5000.yokuba@devbootcamp.com>
2015-08-28 15:43:59 -0400

    add parser

--- source/flashcards_parser.rb
+++ source/flashcards_parser.rb
@@ -1,3 +1,30 @@
-module TextParser
-  File.open
+module CardParser
+  extend self
+
+  def read_file(filepath)
+    array = []
+    File.open(filepath, 'r').each do |line|
+      array << line
+    end
+    array
+  end
+
+  def make_cards(file)
+    deck = []
+    card = {}
+    lines_per_card = 3
+    read_file(file).each_with_index do |line, index|
+      if index % lines_per_card == 0
+        card[:definition] = line
+      elsif index % lines_per_card == 1
+        card[:term] = line
+      elsif index % lines_per_card == 2
+        deck << card
+      end
+    deck
+    end
+  end
+
 end
+
+
